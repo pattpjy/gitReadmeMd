cc4cec5ebdf51a0121e4283c45857f048d14cd55
Patt Sookmark <pijiya@sookmark.com>
2023-07-12 09:52:16 -0600

    Remove console.log

--- script.js
+++ script.js
@@ -158,5 +158,19 @@ console.log(selectedFrontEndFrameworks, selectedBackEndFrameworks);
 function wordToHashtag(word) {
   return `#${word.toLowerCase().replaceAll(" ", "-")}`;
 }
-const testWord = "My Ready Project";
-console.log(wordToHashtag(testWord));
+
+// copy text to clipboard
+var clipboard = new ClipboardJS(".btn");
+
+clipboard.on("success", function (e) {
+  console.info("Action:", e.action);
+  console.info("Text:", e.text);
+  console.info("Trigger:", e.trigger);
+
+  e.clearSelection();
+});
+
+clipboard.on("error", function (e) {
+  console.error("Action:", e.action);
+  console.error("Trigger:", e.trigger);
+});
